717a5e458bb91bf3f8c141b7228cbbd14cc9ab38
John-Web <1196951630@qq.com>
2018-06-23 22:27:47 +0800

    Add files via upload

---
+++ source/Mr.Buck GB/gbcore/GB_SERIALIO.cpp
@@ -0,0 +1,11 @@
+#include "GB_SERIALIO.h"
+void SerialPort::addClock(GB_BY delta) {
+	Clock += delta;
+	if (Clock >= Limit) {
+		Clock -= Limit;
+		Data <<= 1;
+		Data |= 1;//didnt get any message at all.
+		ShiftBit++;
+		if (ShiftBit == 8) { ShiftBit = 0; State = 0; }//automatically end after 1 byte "transfered"
+	}
+}
